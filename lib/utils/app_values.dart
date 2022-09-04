2d6fd2f4feeafb45e8204b93153ac12ffacd976b
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-04 20:06:20 +0900

    コードハイライトの拡張機能を生かすために#をつけた

--- lib/utils/app_values.dart
+++ lib/utils/app_values.dart
@@ -9,19 +9,19 @@ class AppColors {
   static final instance = AppColors._();
 
   // 淡い優しい色
-  static final Color purple = HexColor('cabbe9');
-  static final Color gray = HexColor('696969');
+  static final Color purple = HexColor('#cabbe9');
+  static final Color gray = HexColor('#696969');
   static final Color lightGray = HexColor('#f5f5f5');
-  static final Color white = HexColor('fdfdfd');
+  static final Color white = HexColor('#fdfdfd');
 
   // 強調色
-  static final Color intensePink = HexColor('f469a9');
-  static final Color intensePurple = HexColor('ba53de');
+  static final Color intensePink = HexColor('#f469a9');
+  static final Color intensePurple = HexColor('#ba53de');
 
   // カードに使う色
   static final Color skyGreen = HexColor('#BAD9C8');
   static final Color lightGreen = HexColor('#90DED0');
-  static final Color lightPink = HexColor('ffcef3');
+  static final Color lightPink = HexColor('#ffcef3');
   static final Color salmonPink = HexColor('#FA9CA9');
   static final Color grayPink = HexColor('#B99AA3');
   static final Color brown = HexColor('#F8E0D6');
