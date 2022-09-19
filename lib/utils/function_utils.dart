59c5cf0ce9395fc13407ae3b66ae2e2d549419b1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-20 01:28:12 +0900

    お試しで位置情報確認ダイアログを表示

--- lib/utils/function_utils.dart
+++ lib/utils/function_utils.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
+import 'package:geolocator/geolocator.dart';
 
 /// 汎用的に使用可能なメソッド群
 ///  => 使用方法：FunctionUtils.instance.hogehoge
@@ -19,5 +20,42 @@ class FunctionUtils {
     final image = await boundary.toImage();
     final byteData = await image.toByteData(format: ImageByteFormat.png);
     return byteData?.buffer.asUint8List();
+  }
+
+  /// デバイスの現在位置を決定する。
+  /// 位置情報サービスが有効でない場合、または許可されていない場合。
+  /// エラーを返す
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // 位置情報サービスが有効かどうかをテストします。
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // 位置情報サービスが有効でない場合、続行できません。
+      // 位置情報にアクセスし、ユーザーに対して
+      // 位置情報サービスを有効にするようアプリに要請する。
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // ユーザーに位置情報を許可してもらうよう促す
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // 拒否された場合エラーを返す
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    // 永久に拒否されている場合のエラーを返す
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // ここまでたどり着くと、位置情報に対しての権限が許可されているということなので
+    // デバイスの位置情報を返す。
+    return await Geolocator.getCurrentPosition();
   }
 }
