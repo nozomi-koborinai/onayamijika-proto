2a91c2914faade6c07c404909ebe840a10eb82e1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-20 21:29:33 +0900

    メルボルンからストックホルムに出かけた

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,33 +1,46 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:onayamijika/presentation/components/onayami_card_component.dart';
+import 'package:onayamijika/infrastructure/dtos/onayami_card_document.dart';
 import 'package:onayamijika/presentation/components/seal_making_sheet_model.dart';
 import 'package:onayamijika/presentation/views/onayamijika/onayami_cards_page_view_model.dart';
-import 'package:onayamijika/utils/app_values.dart';
+import 'package:onayamijika/utils/function_utils.dart';
 
 final container = ProviderContainer();
 
 void main() {
-  container.read(selectedOnayamiCardProvider.state).state = OnayamiCardForDisp(
-      cardName: 'testcard',
-      accountImageUrl: 'testurl',
-      accountName: 'testname',
-      distance: 'testkm',
-      content: 'testcontent',
-      cardColor: AppColors.brown);
+  container.read(selectedOnayamiCardDocumentProvider.state).state =
+      const OnayamiCardDocument(
+          cardTitle: '',
+          content: '',
+          latitude: 0.0,
+          longitude: 0.0,
+          createAccountUid: '');
   final OnayamiCardsPageViewModel cardVm =
       container.read(onayamiCardsPageViewModelProvider);
   final SealMakingSheetViewModel sealVm =
       container.read(sealMakingSheetViewModelProvider);
 
+  test('メルボルンからストックホルムへの距離', () async {
+    expect(
+        FunctionUtils.instance.distanceBetween(
+            latitude1: -37.735110047563815,
+            longitude1: 144.96003493198253,
+            latitude2: 59.337692316059865,
+            longitude2: 18.07156327513596),
+        15609534.650715413);
+  });
+
   test('お悩みシール送るシートの初期化時点',
-      () async => {expect(sealVm.targetCard.accountName, 'testname')});
+      () async => {expect(sealVm.targetCard.createAccountUid, 'testname')});
 
   test(
     'カード選択変更時',
     () => {
-      cardVm.onCardIndexChanged(2),
-      expect(container.read(selectedOnayamiCardProvider.state).state.content,
+      expect(
+          container
+              .read(selectedOnayamiCardDocumentProvider.state)
+              .state
+              .content,
           '帰る？')
     },
   );
