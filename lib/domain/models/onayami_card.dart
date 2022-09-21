00dbed53c4396b2ae0cc23b4dabf0185c89dd89a
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-22 01:03:25 +0900

    カードIDを持たせるためにドメイン層用のお悩みカードモデルを定義

---
+++ lib/domain/models/onayami_card.dart
@@ -0,0 +1,7 @@
+import 'package:onayamijika/infrastructure/dtos/onayami_card_document.dart';
+
+class OnayamiCard {
+  final String cardId;
+  final OnayamiCardDocument cardDocument;
+  OnayamiCard({required this.cardId, required this.cardDocument});
+}
