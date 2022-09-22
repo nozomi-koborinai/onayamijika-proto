05c7cb9551a4f076a57dec8e4c065b2506f34113
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-22 11:24:30 +0900

    フィールド名修正

--- lib/domain/models/ solution_seal_view.dart
+++ lib/domain/models/ solution_seal_view.dart
@@ -2,8 +2,8 @@ import 'package:onayamijika/domain/models/%20solution_seal.dart';
 
 /// お悩み解決シール１枚分に必要な情報を一つにまとめた表示用モデル
 class SolutionSealView {
-  List<SolutionSeal> sealDocuments;
+  List<SolutionSeal> seals;
 
   /// コンストラクタ
-  SolutionSealView({required this.sealDocuments});
+  SolutionSealView({required this.seals});
 }
